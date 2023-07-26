357ab5afbb35e29a62fe91e5070d8c9a2e9fee9a
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2023-07-26 03:47:16 +0100

    find solution to array

--- src/app/features/components/views/user/user.component.ts
+++ src/app/features/components/views/user/user.component.ts
@@ -5,58 +5,86 @@ import { Router } from '@angular/router';
 import { GlobalService } from 'src/app/features/services/global.service';
 import { ButtonModule } from 'primeng/button';
 import { TreeNode } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule, TreeTableModule, ButtonModule],
+  imports: [CommonModule, TreeTableModule, ButtonModule, TableModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
   _globalService = inject(GlobalService);
-  _route = inject(Router);
+  // _route = inject(Router);
 
-  demandes!: TreeNode[];
+  // demandes!: TreeNode[];
 
-  ngOnInit() {
-    // this._globalService.getDemandeDetails().subscribe(
-    //   {
-    //     next: (res: any) => {
-    //       console.log('reeeeeeeeeees : ', res.data);
-    //       // this.demandes = res.data.map((elem: any) => {
-    //       //   return [
-    //       //     {
-    //       //       children : [
-    //       //         {
-    //       //           data: elem,
-    //       //           children: [],
-    //       //           parent: null
-    //       //         }
-    //       //       ],
-    //       //       data: elem,
-    //       //       parent: null
-    //       //     }
-    //       //   ]
-    //       // })
-    //     },
-    //     error: (err: any) => {
-    //       console.log(err);
-    //     },
-    //     complete: () => {
-    //       console.log('complete');
-    //     }
-    //   })
+  // ngOnInit() {
+  //   // this._globalService.getDemandeDetails().subscribe(
+  //   //   {
+  //   //     next: (res: any) => {
+  //   //       console.log('reeeeeeeeeees : ', res.data);
+  //   //       // this.demandes = res.data.map((elem: any) => {
+  //   //       //   return [
+  //   //       //     {
+  //   //       //       children : [
+  //   //       //         {
+  //   //       //           data: elem,
+  //   //       //           children: [],
+  //   //       //           parent: null
+  //   //       //         }
+  //   //       //       ],
+  //   //       //       data: elem,
+  //   //       //       parent: null
+  //   //       //     }
+  //   //       //   ]
+  //   //       // })
+  //   //     },
+  //   //     error: (err: any) => {
+  //   //       console.log(err);
+  //   //     },
+  //   //     complete: () => {
+  //   //       console.log('complete');
+  //   //     }
+  //   //   })
 
-    this._globalService.getDemandeDetails().toPromise().then((files) => {
-      console.log('files ==============> ', files)
-      this.demandes = files
-    });
+  //   this._globalService.getDemandeDetails().toPromise().then((files) => {
+  //     console.log('files ==============> ', files)
+  //     this.demandes = files
+  //   });
 
-  }
+  // }
 
-  deleteItem(id: string) {
+  // deleteItem(id: string) {
+  //   console.log('id', id)
+  //   this._globalService.deleteDemande(id).subscribe(
+  //     {
+  //       next: (data: any) => {
+  //         console.log(data);
+  //         this.demandes = this.demandes.filter((elem: any) => elem.data._id !== id);
+  //         // this._route.navigate(['/home/user-interface']);
+  //       },
+  //       error: (err: any) => {
+  //         console.log(err);
+  //       },
+  //       complete: () => {
+  //         console.log('complete');
+  //       }
+  //     })
+  // }
+  products!: any[];
+  demandes!: any[];
+
+    ngOnInit() {
+      this._globalService.getProductsWithOrdersSmall().then((data) => {
+        console.log('data =====> ', data)
+        this.products = data
+    })}
+
+      deleteItem(id: string) {
     console.log('id', id)
     this._globalService.deleteDemande(id).subscribe(
       {
--- src/app/features/services/global.service.ts
+++ src/app/features/services/global.service.ts
@@ -72,10 +72,10 @@ export class GlobalService {
         );
     }
 
-    successLogin(userData:LoginDto, access_token: string) {
+    successLogin(userData: LoginDto, access_token: string) {
         localStorage.setItem(AccessTokenLocalStorage, access_token);
         this.currentAccessTokenSubject.next(access_token);
-        this._store.dispatch(setConnectedUser({user: userData}))
+        this._store.dispatch(setConnectedUser({ user: userData }))
     }
 
     logout() {
@@ -113,321 +113,38 @@ export class GlobalService {
     //     'Something bad happened; please try again later.');
     // };
 
-    getFileSystemNodesData() {
+    getProductsWithOrdersData() {
         return [
             {
-                "data": {
-                    "name": "Applications",
-                    "size": "200mb",
-                    "type": "Folder"
-                },
-                "children": [
+                id: '1000',
+                email: 'f230fh0g3bbbb',
+                name: 'Bamboo Watchs',
+                description: 'Product Descrifffffffffption',
+                type: 'Accessories',
+                orders: [
                     {
-                        "data": {
-                            "name": "Angular",
-                            "size": "25mb",
-                            "type": "Folder"
-                        },
-                        "children": [
-                            {
-                                "data": {
-                                    "name": "angular.app",
-                                    "size": "10mb",
-                                    "type": "Application"
-                                }
-                            },
-                            {
-                                "data": {
-                                    "name": "cli.app",
-                                    "size": "10mb",
-                                    "type": "Application"
-                                }
-                            },
-                            {
-                                "data": {
-                                    "name": "mobile.app",
-                                    "size": "5mb",
-                                    "type": "Application"
-                                }
-                            }
-                        ]
+                        review: '1000-0',
                     },
-                    {
-                        "data": {
-                            "name": "editor.app",
-                            "size": "25mb",
-                            "type": "Application"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "settings.app",
-                            "size": "50mb",
-                            "type": "Application"
-                        }
-                    }
                 ]
             },
             {
-                "data": {
-                    "name": "Cloud",
-                    "size": "20mb",
-                    "type": "Folder"
-                },
-                "children": [
+                id: '1000',
+                email: 'f230fh0g3aaaaaa',
+                name: 'Bamboo Watch',
+                description: 'Product Description',
+                type: 'Accessoriess',
+                orders: [
                     {
-                        "data": {
-                            "name": "backup-1.zip",
-                            "size": "10mb",
-                            "type": "Zip"
-                        }
+                        review: '1000-0',
                     },
-                    {
-                        "data": {
-                            "name": "backup-2.zip",
-                            "size": "10mb",
-                            "type": "Zip"
-                        }
-                    }
                 ]
             },
-            {
-                "data": {
-                    "name": "Desktop",
-                    "size": "150kb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "note-meeting.txt",
-                            "size": "50kb",
-                            "type": "Text"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "note-todo.txt",
-                            "size": "100kb",
-                            "type": "Text"
-                        }
-                    }
-                ]
-            },
-            {
-                "data": {
-                    "name": "Documents",
-                    "size": "75kb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "Work",
-                            "size": "55kb",
-                            "type": "Folder"
-                        },
-                        "children": [
-                            {
-                                "data": {
-                                    "name": "Expenses.doc",
-                                    "size": "30kb",
-                                    "type": "Document"
-                                }
-                            },
-                            {
-                                "data": {
-                                    "name": "Resume.doc",
-                                    "size": "25kb",
-                                    "type": "Resume"
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        "data": {
-                            "name": "Home",
-                            "size": "20kb",
-                            "type": "Folder"
-                        },
-                        "children": [
-                            {
-                                "data": {
-                                    "name": "Invoices",
-                                    "size": "20kb",
-                                    "type": "Text"
-                                }
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                "data": {
-                    "name": "Downloads",
-                    "size": "25mb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "Spanish",
-                            "size": "10mb",
-                            "type": "Folder"
-                        },
-                        "children": [
-                            {
-                                "data": {
-                                    "name": "tutorial-a1.txt",
-                                    "size": "5mb",
-                                    "type": "Text"
-                                }
-                            },
-                            {
-                                "data": {
-                                    "name": "tutorial-a2.txt",
-                                    "size": "5mb",
-                                    "type": "Text"
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        "data": {
-                            "name": "Travel",
-                            "size": "15mb",
-                            "type": "Text"
-                        },
-                        "children": [
-                            {
-                                "data": {
-                                    "name": "Hotel.pdf",
-                                    "size": "10mb",
-                                    "type": "PDF"
-                                }
-                            },
-                            {
-                                "data": {
-                                    "name": "Flight.pdf",
-                                    "size": "5mb",
-                                    "type": "PDF"
-                                }
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                "data": {
-                    "name": "Main",
-                    "size": "50mb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "bin",
-                            "size": "50kb",
-                            "type": "Link"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "etc",
-                            "size": "100kb",
-                            "type": "Link"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "var",
-                            "size": "100kb",
-                            "type": "Link"
-                        }
-                    }
-                ]
-            },
-            {
-                "data": {
-                    "name": "Other",
-                    "size": "5mb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "todo.txt",
-                            "size": "3mb",
-                            "type": "Text"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "logo.png",
-                            "size": "2mb",
-                            "type": "Picture"
-                        }
-                    }
-                ]
-            },
-            {
-                "data": {
-                    "name": "Pictures",
-                    "size": "150kb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "barcelona.jpg",
-                            "size": "90kb",
-                            "type": "Picture"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "primeng.png",
-                            "size": "30kb",
-                            "type": "Picture"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "prime.jpg",
-                            "size": "30kb",
-                            "type": "Picture"
-                        }
-                    }
-                ]
-            },
-            {
-                "data": {
-                    "name": "Videos",
-                    "size": "1500mb",
-                    "type": "Folder"
-                },
-                "children": [
-                    {
-                        "data": {
-                            "name": "primefaces.mkv",
-                            "size": "1000mb",
-                            "type": "Video"
-                        }
-                    },
-                    {
-                        "data": {
-                            "name": "intro.avi",
-                            "size": "500mb",
-                            "type": "Video"
-                        }
-                    }
-                ]
-            }
-        ]
+
+        ];
     }
 
-    getFilesystem() {
-        return Promise.resolve(this.getFileSystemNodesData());
+    getProductsWithOrdersSmall() {
+        return Promise.resolve(this.getProductsWithOrdersData().slice(0, 10));
     }
 
 
